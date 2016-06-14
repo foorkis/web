1bd107857bfe95daa4f68e58b8ee95ef1af93b6a
foorkis <foorkis@gmail.com>
2016-06-14 10:58:04 +0300

    one big fix

--- l8/lab.js
+++ l8/lab.js
@@ -1,41 +1,41 @@
 $(document).ready(function() {
- $('#myForm').validate({
+    $('#myForm').validate({
 	rules:{
-    login:{ required:true,
-          rangelength:[4,8]},
-	email:{
+	    login:{ required:true,
+		    rangelength:[4,8]},
+	    email:{
 		required:true,
 		email:true	
-		},
-	pass:{
-	    required:true,
+	    },
+	    pass:{
+		required:true,
 		rangelength:[4,8]	
+	    },
+	    repass:{
+		equalTo:'#pass'	
+	    }
 	},
-	repass:{
-	equalTo:'#pass'	
-	}
-	},//конец rules
 	messages:{
-        login:{
-        required: "Вы не ввели логин",
-	   rangelength:"Логин должен содержать от 4 до 8 символов"
-        },
-	email:{
-	required: "Вы не ввели адрес электронной почты",
-	email: "Вы ввели некорректный e-mail"	
+            login:{
+		required: "Вы не ввели логин",
+		rangelength:"Логин должен содержать от 4 до 8 символов"
+            },
+	    email:{
+		required: "Вы не ввели адрес электронной почты",
+		email: "Вы ввели некорректный e-mail"	
+	    },
+	    pass:{
+		required: "Вы не ввели пароль",
+		rangelength:"Пароль должен содержать от 4 до 8 символов"	
+	    },
+	    repass:{
+		equalTo:"Пароли не совпадают"	
+	    }
 	},
-	pass:{
-	required: "Вы не ввели пароль",
-	rangelength:"Пароль должен содержать от 4 до 8 символов"	
-	},
-	repass:{
-	equalTo:"Пароли не совпадают"	
-	}
-	},//конец messages
 	errorPlacement:function(error,element){
-	
-	error.insertAfter(element);	
-	
-	}//конец errorPlacement
- });//конец validate    
-});//конец ready
+	    
+	    error.insertAfter(element);	
+	    
+	}
+    });    
+});
